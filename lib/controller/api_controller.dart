33dc0e19258cc20a50cf4ad825a8d11441dafa31
R@jput Nir@j <nirajrajput6983@gmail.com>
2024-07-21 20:30:14 +0530

    21-07-2024

--- lib/controller/api_controller.dart
+++ lib/controller/api_controller.dart
@@ -1,19 +1,48 @@
+// import 'dart:convert';
+// import 'package:get/get.dart';
+// import 'package:validation_api_calling_getx/controller/api_sevices.dart';
+// import 'package:validation_api_calling_getx/modal/Api_model.dart';
+//
+// class ApiController extends GetxController{
+//   late Comments comments;
+//   Future<void> fetchData() async {
+//     ApiServices apiServices = ApiServices();
+//     String? data = await  apiServices.apiCalling();
+//     Map json = jsonDecode(data!);
+//     comments = Comments.fromJson(json);
+//
+//   }
+//   ApiController()
+//   {
+//     fetchData();
+//   }
+// }
+
 import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:validation_api_calling_getx/controller/api_sevices.dart';
 import 'package:validation_api_calling_getx/modal/Api_model.dart';
 
-class ApiController extends GetxController{
-  late Comments comments;
-  Future<void> fetchData() async {
-    ApiServices apiServices = ApiServices();
-    String? data = await  apiServices.apiCalling();
-    Map json = jsonDecode(data!);
-    comments = Comments.fromJson(json);
+class ApiController extends GetxController {
+  var comments = Comments().obs;
+  var isLoading = true.obs;
 
+  Future<void> fetchData() async {
+   ApiServices apiServices = ApiServices();
+    String? data = await apiServices.apiCalling();
+    if (data != null) {
+      Map<String, dynamic> json = jsonDecode(data);
+      comments.value = Comments.fromJson(json);
+      isLoading.value = false;
+    } else {
+      // Handle error here
+      isLoading.value = false;
+    }
   }
-  ApiController()
-  {
+
+  @override
+  void onInit() {
     fetchData();
+    super.onInit();
   }
 }
--- lib/controller/api_sevices.dart
+++ lib/controller/api_sevices.dart
@@ -16,3 +16,4 @@ class ApiServices {
     return null;
   }
 }
+
